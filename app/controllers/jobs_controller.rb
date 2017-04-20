63e612c59d527df8326a89c13ba63badfd59ecf7
sunshinehope <1355251612@qq.com>
2017-04-20 16:03:09 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,12 +1,16 @@
 class JobsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-    before_action :require_is_admin
+    # before_action :require_is_admin
   def index
    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def show
   @job = Job.find(params[:id])
+  if @job.is_hidden
+  flash[:warning] = "This Job already archived"
+  redirect_to root_path
+end
 end
 def new
   @job = Job.new
